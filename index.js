58a9e8d2e67eecfd7a28a3cc05442ec2aeebcbfe
Ashley Knorr Gehring <ashleyknorr@gmail.com>
2021-04-07 17:11:14 -0500

    commit tree cbfedde78f1f7d2f9014eba35f30cb51aa592792

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution in this file!
+
+const HQ = 42
+function distanceFromHqInBlocks(pickupLocation) {
+    if (pickupLocation >= 42) {
+        return pickupLocation - HQ;
+    }
+    else {
+        return HQ - pickupLocation 
+    }
+};
+function distanceFromHqInFeet(pickupLocation){
+    if (pickupLocation >= 42) {
+        return (pickupLocation - HQ)*264;
+    }
+    else {
+        return (HQ - pickupLocation)*264;
+    }
+};
+
+function  distanceTravelledInFeet(start, end){
+    if (end >= start){
+        return  (end - start)*264;
+}
+else {
+    return (start-end)*264
+}
+};
+
+function calculatesFarePrice(start, end){
+    let distance = distanceTravelledInFeet(start, end);
+    if (distance<400){
+        return 0
+    }
+    else if (distance > 400 && distance<2000){
+        return ((distance-400)*0.02)
+    }
+    else if (distance>=2000 && distance < 2500){
+        return 25
+    }
+    else if (distance>2500){
+        return "cannot travel that far"
+    }
+};
